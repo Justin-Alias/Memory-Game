2495f71b98d2c96013329fe7de4761d9c18ae60d
Period Three <periodthree@imac-25.local>
2017-11-07 12:06:06 -0500

    Initial Commit

---
+++ Sousa_Justin_Memory_Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sousa_Justin_Memory_Game
+//
+//  Created by Period Three on 2017-11-07.
+//  Copyright © 2017 Period Three. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
